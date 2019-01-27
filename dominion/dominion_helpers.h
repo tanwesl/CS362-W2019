4910461f5488f7ea217f3b2937a701948f93c83c
Wesley Tan <tanwe@flip2.engr.oregonstate.edu>
2019-01-27 01:41:00 -0800

    Assignment 2 Submission
    Refactored code for Smithy, Adventurer, Feast, Tribute, Sea Hag
    
    Bugs added to Smithy, Adventurer, Feast, Sea Hag

--- dominion/dominion_helpers.h
+++ dominion/dominion_helpers.h
@@ -9,6 +9,7 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
+void adventurerCard(struct gameState *state);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
